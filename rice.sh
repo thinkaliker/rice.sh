22f1e4c878489631c498e9124b2dc0989565444f
Adam <adam@thinkaliker.com>
2017-09-01 22:23:54 -0700

    added expandable logic, prompt, additional script selection

--- rice.sh
+++ rice.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 VERSION="0.1"
-SCRIPT[1]="rice-vim"
-SCRIPT[2]="rice-update"
-SCRIPT[3]="rice-ailias"
+SCRIPT=("rice-vim" "rice-update" "rice-dev")
+
 echo " > Welcome to          ";
 echo "      _                _     ";
 echo " _ __(_) ___ ___   ___| |__  ";
@@ -14,11 +13,34 @@ echo " > Version $VERSION";
 echo ""; 
 echo "By default, rice.sh will install: "
 # Default script for loop
-for index in 1 2 3
-    do printf " > %s\n" "${SCRIPT[index]}"
+DEFAULTS=$((2))
+for (( i=0; i<$DEFAULTS; i++)) ; do
+    printf " > %s\n" "${SCRIPT[$i]}"
 done
 echo "By default, rice.sh will overwrite any existing files.";
-# Loader for loop - modify this if you are adding more scripts
-for index in 1 2 3
-    do ./${SCRIPT[index]}/${SCRIPT[index]}.sh
+for (( i=0; i<$DEFAULTS; i++)) ; do
+    ./${SCRIPT[$i]}/${SCRIPT[$i]}.sh
+done
+
+while true ; do
+    ARRSIZE=$((${#SCRIPT[@]}))
+    ADDITIONALINPUT=0
+    SELECTINPUT=0
+    read -p "Install additional scripts? (y/n): " ADDITIONALINPUT
+    if [ $ADDITIONALINPUT = "y" ] || [ $ADDITIONALINPUT = "Y" ] ; then
+        echo "Choose a script to run: ";
+        for (( i=0; i<$ARRSIZE; i++)) ; do
+            printf " [%u] %s\n" $i ${SCRIPT[i]};
+        done
+        printf "Script selection: ";
+        read SELECTINPUT
+        if [ $SELECTINPUT -lt $ARRSIZE ]; then
+            ./${SCRIPT[SELECTINPUT]}/${SCRIPT[SELECTINPUT]}.sh
+        else
+            echo "/!\ Invalid selection.";
+        fi
+    else
+        echo "Goodbye.";
+        exit;
+    fi
 done
