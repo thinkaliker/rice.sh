793048c78fe1c59d793598c2af9afaeab9433a9d
Adam <adam@thinkaliker.com>
2017-09-01 20:36:59 -0700

    rice-update init commit

---
+++ qup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+GRN='\033[0;32m'
+NC='\033[0m'
+
+echo -e "${GRN}[quick update (qup)]${NC}"
+echo -e "${GRN}[updating repositories]${NC}"
+sudo apt-get update
+echo -e "${GRN}[upgrading packages]${NC}"
+sudo apt-get upgrade -y
+echo -e "${GRN}[dist-upgrade packages]${NC}"
+sudo apt-get dist-upgrade -y
+echo -e "${GRN}[autoremove packages]${NC}"
+sudo apt-get autoremove -y
+echo -e "${GRN}[qup complete]${NC}"
---
+++ rice-update.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "=================================";
+echo " Ricing out: update";
+echo " Distro: Ubuntu";
+#TODO add more distro support
+echo " > installing qup to /bin";
+if [ -f /bin/qup ] ; then
+echo " qup already installed, upgrading"
+else
+if chmod +x ./qup.sh ; then 
+if sudo cp ./qup.sh /bin/qup ; then
+echo " qup successfully installed."
+echo " Quick reference:"
+echo "  > run 'qup' to automatically update all currently installed packages"
+else 
+echo " /!\ rice-update failed to copy."
+fi
+else
+echo " /!\ rice-update failed to change permissions." 
+fi
+fi
+echo "=================================";
