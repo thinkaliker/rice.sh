2b61442bd3b48d73e7b6fa22415f8baf0d75d4d3
Adam <adam@thinkaliker.com>
2018-04-11 23:33:41 -0700

    formatting and redundant copy fix

--- rice-update/rice-update.sh
+++ rice-update/rice-update.sh
@@ -6,18 +6,20 @@ echo " Distro: Ubuntu";
 #TODO add more distro support
 echo " > installing qup to /bin";
 if [ -f /bin/qup ] ; then
-echo " qup already installed, upgrading"
-else
-if chmod +x ./qup.sh ; then 
-if sudo cp ./qup.sh /bin/qup ; then
-echo " qup successfully installed."
-echo " Quick reference:"
-echo "  > run 'qup' to automatically update all currently installed packages"
-else 
-echo " /!\ rice-update failed to copy."
-fi
-else
-echo " /!\ rice-update failed to change permissions." 
-fi
+    echo " qup already installed, upgrading"
+    if sudo cp ./qup.sh /bin/qup ; then
+        sudo cp ./qup.sh /bin/qup
+    else
+        if chmod +x ./qup.sh ; then 
+            if sudo cp ./qup.sh /bin/qup ; then
+            echo " qup successfully installed."
+            echo " Quick reference:"
+            echo "  > run 'qup' to automatically update all currently installed packages"
+        else 
+            echo " /!\ rice-update failed to copy."
+        fi
+    else
+        echo " /!\ rice-update failed to change permissions." 
+    fi
 fi
 echo "=================================";
